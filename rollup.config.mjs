448ae8982efe85ddfc5ab4ff827a8a2ed5035042
Black <86022898+Bonny-kato@users.noreply.github.com>
2024-02-16 23:17:39 +0300

    chore: initial commit

---
+++ rollup.config.mjs
@@ -0,0 +1,36 @@
+import dts from "rollup-plugin-dts";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+
+import packageJson from "./package.json" assert { type: "json" };
+import typescript from "@rollup/plugin-typescript";
+
+export default [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: packageJson.main,
+                format: "cjs",
+                sourcemap: true,
+            },
+            {
+                file: packageJson.module,
+                format: "esm",
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            terser(), // A Rollup plugin to generate a minified bundle with terser.
+            resolve(), // Lets Rollup find 'node_modules' packages when bundling dependencies
+            commonjs(), // Converts CommonJS modules to ES6 format so they can be included in a Rollup bundle
+            typescript({tsconfig: "./tsconfig.json"}), // Transpiles TypeScript code to JavaScript
+        ],
+    },
+    {
+        input: "dist/esm/types/index.d.ts",
+        output: [{file: "dist/index.d.ts", format: "esm"}],
+        plugins: [dts()], // Generates a bundled .d.ts file from TypeScript declaration files
+    },
+];
---
+++ src/README.md
@@ -0,0 +1,44 @@
+## Package Boilerplate
+
+# Starter-Library
+
+Starter-Library is here to ease your library development process. It is perfect for your TypeScript and JavaScript
+projects offering support for both ts types and JS files. This library uses Rollup.js as its module bundler.
+
+## Features
+
+- Supports Rollup.js for module bundling
+- Generates types for TypeScript
+- Can be run on both TypeScript and JavaScript projects
+- Offers minified output
+- Supports alias import
+- Ensures compatibility with CommentJs and ES modules
+- Integrated with git hooks for automating tasks on git events
+
+## Installation
+
+Use your favorite package manager to install the Starter Library.
+
+```
+npm install starter-library
+```
+
+## Usage
+
+To use this library in your project, you can require it in your JavaScript/TypeScript file.
+
+```
+const starterLibrary = require('starter-library');
+```
+
+## Contributing
+
+Pull requests are welcome.
+
+## License
+
+MIT
+
+## Contact
+
+For more information and queries, please contact us.
